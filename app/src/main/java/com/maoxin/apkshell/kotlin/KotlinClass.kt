3c5a817eeb626820aa9b2300e944eec2085bab4b
maoxin <linwinfil@gmail.com>
2019-02-18 00:05:32 +0800

    基础-类

---
+++ app/src/main/java/com/maoxin/apkshell/kotlin/KotlinClass.kt
@@ -0,0 +1,14 @@
+package com.maoxin.apkshell.kotlin
+
+/** @author lmx
+ * Created by lmx on 2019/2/17.
+ * @see https://www.kotlincn.net/docs/reference/classes.html
+ */
+class KotlinClass {
+
+    //没有类体的class，可以不需要花括号
+    class ClassEmpth
+
+    //构造函数使用constructor申明，也可以省略
+    class Person constructor(name: String)
+}
